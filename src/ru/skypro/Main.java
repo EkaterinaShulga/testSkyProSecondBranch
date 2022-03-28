6603f9abff9200823309ea2dc9042acfb578f31a
Екатерина <shulga23@gmail.com>
2022-03-28 13:02:09 +0300

    готовое дз от Шульга Екатерины

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,6 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Первый проект, который будет сохранен на GitHub!");
     }
 }
