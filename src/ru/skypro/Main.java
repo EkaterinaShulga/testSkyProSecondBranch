d971b96492009706a32f933f16bdce05453d4aeb
Екатерина <shulga23@gmail.com>
2022-03-28 13:58:32 +0300

    готовое дз от Шульга Екатерины

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Первое изменение, которое будет сохранено на GitHub!");
-        System.out.println("С первого раза не получилось!");
 
     }
 }
